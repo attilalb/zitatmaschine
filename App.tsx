2c01b371895e3b2e5cace32a8cc93040c859be51
attilalb <115696445+attilalb@users.noreply.github.com>
2023-04-28 19:11:43 +0200

    added footer

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import './style.css';
 import Wrapper from './components/Wrapper';
 import { useState } from 'react';
+import { FcLike } from 'react-icons/fc';
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -9,7 +10,7 @@ export default function App() {
 
   return (
     <div className="p-6 content">
-      <h1 className="has-text-centered">QuoteMachine</h1>
+      <h1 className="has-text-centered mb-5">QuoteMachine</h1>
       <Wrapper
         isLoading={isLoading}
         onLoad={setIsLoading}
@@ -18,6 +19,15 @@ export default function App() {
         setQuote={setQuote}
         setAuthor={setAuthor}
       />
+      <footer className="level">
+        <p className="is-size-7 has-text-centered mt-0 pt-0">
+          Made with{' '}
+          <span class="icon">
+            <FcLike />
+          </span>{' '}
+          by <a href="https://github.com/attilalb/">Attila</a>.
+        </p>
+      </footer>
     </div>
   );
 }
